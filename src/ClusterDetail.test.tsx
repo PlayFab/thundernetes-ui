a1958c0fec171d464b2d793e9212b41ff83a5a22
Javier Morales <javiermoralesr95@gmail.com>
2022-06-01 17:43:33 -0400

    Minor refactor

--- src/ClusterDetail.test.tsx
+++ src/ClusterDetail.test.tsx
@@ -3,8 +3,9 @@ import { setupServer } from "msw/node";
 import { MemoryRouter, Route, Routes } from "react-router-dom";
 import { render, screen } from '@testing-library/react';
 import ClusterDetail from './ClusterDetail';
+import { GameServer, GameServerBuild } from "./types";
 
-const gameServerBuildsResponse = [
+const gameServerBuilds: Array<GameServerBuild> = [
   {
     apiVersion: "",
     kind: "",
@@ -31,7 +32,7 @@ const gameServerBuildsResponse = [
   }
 ]
 
-const gameServersResponse = [
+const gameServers: Array<GameServer> = [
   {
     metadata: {
       name: "test_gameserver1",
@@ -63,13 +64,13 @@ const gameServersResponse = [
 const server = setupServer(
   rest.get("http://thundernetescluster1:5001/api/v1/gameserverbuilds", (req, res, ctx) => {
     return res(ctx.json({
-      items: gameServerBuildsResponse
+      items: gameServerBuilds
     }))
   }),
 
   rest.get("http://thundernetescluster1:5001/api/v1/gameservers", (req, res, ctx) => {
     return res(ctx.json({
-      items: gameServersResponse
+      items: gameServers
     }))
   }),
 );
@@ -94,24 +95,24 @@ afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
 it('shows a table with gameserverbuilds', async () => {
-  let gsbRowValuesString = gameServerBuildsResponse[0].metadata.name;
-  gsbRowValuesString += " " + gameServerBuildsResponse[0].spec.buildID;
-  gsbRowValuesString += " " + gameServerBuildsResponse[0].metadata.namespace;
-  gsbRowValuesString += " " + gameServerBuildsResponse[0].status.currentActive;
-  gsbRowValuesString += " " + gameServerBuildsResponse[0].status.currentStandingByReadyDesired;
-  gsbRowValuesString += " " + gameServerBuildsResponse[0].status.crashesCount;
-  gsbRowValuesString += " " + gameServerBuildsResponse[0].status.health;
+  let gsbRowValuesString = gameServerBuilds[0].metadata.name;
+  gsbRowValuesString += " " + gameServerBuilds[0].spec.buildID;
+  gsbRowValuesString += " " + gameServerBuilds[0].metadata.namespace;
+  gsbRowValuesString += " " + gameServerBuilds[0].status.currentActive;
+  gsbRowValuesString += " " + gameServerBuilds[0].status.currentStandingByReadyDesired;
+  gsbRowValuesString += " " + gameServerBuilds[0].status.crashesCount;
+  gsbRowValuesString += " " + gameServerBuilds[0].status.health;
   const gsbRow = await screen.findByRole("row", { name: new RegExp(gsbRowValuesString) });
   expect(gsbRow).toBeInTheDocument();
 });
 
 it('shows a table with nodes', async () => {
-  let nodeRowValuesString1 = gameServersResponse[0].status.nodeName;
+  let nodeRowValuesString1 = gameServers[0].status.nodeName;
   nodeRowValuesString1 += " 1 0";
   const nodeRow1 = await screen.findByRole("row", { name: new RegExp(nodeRowValuesString1) });
   expect(nodeRow1).toBeInTheDocument();
 
-  let nodeRowValuesString2 = gameServersResponse[1].status.nodeName;
+  let nodeRowValuesString2 = gameServers[1].status.nodeName;
   nodeRowValuesString2 += " 0 1";
   const nodeRow2 = await screen.findByRole("row", { name: new RegExp(nodeRowValuesString2) });
   expect(nodeRow2).toBeInTheDocument();
