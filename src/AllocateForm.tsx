b27101d36ce436b4dea59e5103ca14ba3ea54cd3
Javier Morales <javiermoralesr95@gmail.com>
2022-06-02 17:52:55 -0400

    Added errors to the allocate form

--- src/AllocateForm.tsx
+++ src/AllocateForm.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, Grid, TextField, Typography } from "@mui/material";
+import { Box, Button, Chip, Grid, Stack, TextField, Typography } from "@mui/material";
 
 interface AllocateFormProps {
   allocateApi: string,
@@ -10,6 +10,8 @@ function AllocateForm({ allocateApi, buildID }: AllocateFormProps) {
   const [sessionID, setSessionID] = useState("");
   const [assignedIP, setAssignedIP] = useState("");
   const [assignedPorts, setAssignedPorts] = useState("");
+  const [allocateError, setAllocateError] = useState<Error>();
+  const [requestDenied, setRequestDenied] = useState(false);
 
   const handleChange = (event: any) => {
     if (event.target.name === "sessionID") {
@@ -29,27 +31,60 @@ function AllocateForm({ allocateApi, buildID }: AllocateFormProps) {
         "Content-Type": "application/json"
       },
       body: JSON.stringify(body)
-    }).then(response => response.json())
-      .then(response => { setAssignedIP(response.IPV4Address); setAssignedPorts(response.Ports); });
+    }).then(response => {
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        setRequestDenied(true);
+        return undefined;
+      }
+    }).then(response => {
+      if (response) {
+        setAssignedIP(response.IPV4Address);
+        setAssignedPorts(response.Ports);
+      }
+    }).catch(err => {
+      setAllocateError(err);
+    });
   }
 
   return (
     <form onSubmit={handleSubmit}>
-        <Grid container justifyContent="left" spacing={2} sx={{ flexGrow: 1, marginBottom: "40px" }}>
-          <Grid item xs={5}>
-            <TextField fullWidth name="sessionID" size="small" label="SessionID" value={sessionID} onChange={handleChange} />
-          </Grid>
-          <Grid item xs={7}>
-            <Button variant="contained" color="primary" type="submit">
-              Allocate
-            </Button>
-          </Grid>
-          {(assignedIP && assignedPorts) &&
-            <Grid item xs={5}>
-              <Typography>IP={assignedIP}, Port={assignedPorts}</Typography>
-            </Grid>
-          }
+      <Grid container justifyContent="left" spacing={2} sx={{ flexGrow: 1, marginBottom: "40px" }}>
+        <Grid item xs={5}>
+          <TextField fullWidth name="sessionID" size="small" label="SessionID" value={sessionID} onChange={handleChange} />
         </Grid>
+        <Grid item xs={7}>
+          <Button variant="contained" color="primary" type="submit">
+            Allocate
+          </Button>
+        </Grid>
+        {(!allocateError && !requestDenied &&assignedIP && assignedPorts) &&
+          <Grid item xs={12}>
+            <Typography>IP={assignedIP}, Port={assignedPorts}</Typography>
+          </Grid>
+        }
+        {(allocateError) &&
+          <Grid item xs={12}>
+            <Box display="flex" justifyContent="left">
+              <Stack direction="column">
+                <Chip color="error" sx={{ marginBottom: "5px" }} variant="outlined"
+                  label={"Couldn't reach allocation endpoint at: " + allocateApi} />
+              </Stack>
+            </Box>
+          </Grid>
+        }
+        {(requestDenied) &&
+          <Grid item xs={12}>
+            <Box display="flex" justifyContent="left">
+              <Stack direction="column">
+                <Chip color="error" sx={{ marginBottom: "5px" }} variant="outlined"
+                  label={"Allocation endpoint denied the request at: " + allocateApi} />
+              </Stack>
+            </Box>
+          </Grid>
+        }
+      </Grid>
     </form>
   );
 }
