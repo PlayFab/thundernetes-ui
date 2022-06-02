20e6c38ced8c125288f84ef1668057e8116f7c4a
Javier Morales <javiermoralesr95@gmail.com>
2022-06-02 17:16:48 -0400

    Added error messages in GameServerBuildDetail

--- src/GameServerBuildDetail.tsx
+++ src/GameServerBuildDetail.tsx
@@ -1,22 +1,24 @@
+import React from "react";
 import { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Box, Typography } from "@mui/material";
+import { Box, Chip, Stack, Typography } from "@mui/material";
 import AllocateForm from "./AllocateForm";
 import GameServerBuildSpec from "./GameServerBuildSpec";
 import GameServerBuildStatus from "./GameServerBuildStatus"
 import GameServerTable from "./GameServerTable";
 import NodeTable from "./NodeTable";
 import SpecForm from "./SpecForm";
-import { emptyGameServerBuild, GameServerBuild, GameServer, GameServerDetail } from "./types";
+import { GameServerBuild, GameServer, GameServerDetail } from "./types";
 
 interface GameServerBuildDetailProps {
   clusters: Record<string, Record<string, string>>
 }
 
 function GameServerBuildDetail({ clusters }: GameServerBuildDetailProps) {
-  const [gsb, setGsb] = useState<GameServerBuild>(emptyGameServerBuild);
+  const [gsb, setGsb] = useState<GameServerBuild>();
   const [gsList, setGsList] = useState<Array<GameServer>>([]);
   const [gsdList, setGsdList] = useState<Array<GameServerDetail>>([]);
+  const [apiError, setApiError] = useState<Error>();
 
   const params = useParams();
   const clusterName = params.clusterName ? params.clusterName : "";
@@ -27,21 +29,27 @@ function GameServerBuildDetail({ clusters }: GameServerBuildDetailProps) {
     fetch(clusterApi + "gameserverbuilds/" + params.namespace + "/" + params.buildName + "/gameservers")
       .then(response => response.json())
       .then(response => setGsList(response.items))
-      .catch(err => console.log(err));
+      .catch(err => setApiError(err));
   }, [clusterApi, params.namespace, params.buildName]);
 
   const getGameServerBuild = useCallback(() => {
     fetch(clusterApi + "gameserverbuilds/" + params.namespace + "/" + params.buildName)
-      .then(response => response.json())
+      .then(response => {
+        if (response.status === 200) {
+          return response.json();
+        } else if (response.status === 404) {
+          return undefined;
+        }
+      })
       .then(response => setGsb(response))
-      .catch(err => setGsb(emptyGameServerBuild));
+      .catch(err => setApiError(err));
   }, [clusterApi, params.namespace, params.buildName]);
 
   const getGameServerDetails = useCallback(() => {
     fetch(clusterApi + "gameserverbuilds/" + params.namespace + "/" + params.buildName + "/gameserverdetails")
       .then(response => response.json())
       .then(response => setGsdList(response.items))
-      .catch(err => console.log(err));
+      .catch(err => setApiError(err));
   }, [clusterApi, params.namespace, params.buildName]);
 
   const groupDataByNode = (gsList: Array<GameServer>) => {
@@ -88,34 +96,54 @@ function GameServerBuildDetail({ clusters }: GameServerBuildDetailProps) {
 
   const nodeData = groupDataByNode(gsList);
   const gsdByName = groupDetailsByName(gsdList);
-  
+
   return (
-    <Box>
-      <Typography variant="h4" gutterBottom component="div" sx={{ marginBottom: "40px" }}>
-        {params.namespace + "/" + params.buildName}
-      </Typography>
-      <Typography variant="h6" gutterBottom component="div" sx={{ marginBottom: "20px" }}>
-        Specs
-      </Typography>
-      <Box sx={{ marginBottom: "40px" }}><GameServerBuildSpec gsb={gsb} /></Box>
-      <Typography variant="h6" gutterBottom component="div" sx={{ marginBottom: "20px" }}>
-        Patch Specs
-      </Typography>
-      <Box sx={{ marginBottom: "40px" }}><SpecForm clusterApi={clusterApi} gsb={gsb} /></Box>
-      <Typography variant="h6" gutterBottom component="div" sx={{ marginBottom: "20px" }}>
-        Status
-      </Typography>
-      <Box sx={{ marginBottom: "40px" }}><GameServerBuildStatus gsb={gsb} /></Box>
-      <Typography variant="h6" gutterBottom component="div" sx={{ marginBottom: "20px" }}>
-        Nodes
-      </Typography>
-      <Box sx={{ marginBottom: "40px" }}><NodeTable nodeData={nodeData} /></Box>
-      <Typography variant="h6" gutterBottom component="div" sx={{ marginBottom: "20px" }}>
-        Game Servers
-      </Typography>
-      <Box><AllocateForm allocateApi={allocateApi} buildID={gsb.spec ? gsb.spec.buildID : ""} /></Box>
-      <Box><GameServerTable gsList={gsList} gsdByName={gsdByName} /></Box>
-    </Box>
+    <React.Fragment>
+      {(apiError) &&
+        <Box display="flex" justifyContent="center">
+          <Stack direction="column">
+            <Chip color="error" sx={{ marginBottom: "5px" }} variant="outlined"
+              label={"Couldn't reach cluster '" + clusterName + "' at: " + clusters[clusterName].api} />
+          </Stack>
+        </Box>
+      }
+      {(!gsb && !apiError) &&
+        <Box display="flex" justifyContent="center">
+          <Stack direction="column">
+            <Chip color="error" sx={{ marginBottom: "5px" }} variant="outlined"
+              label={"Couldn't find Build with name '" + params.buildName + "' in namespace '" + params.namespace + "'"} />
+          </Stack>
+        </Box>
+      }
+      {(gsb) &&
+        <React.Fragment>
+          <Typography variant="h4" gutterBottom component="div" sx={{ marginBottom: "40px" }}>
+            {gsb.metadata.namespace + "/" + gsb.metadata.name}
+          </Typography>
+          <Typography variant="h6" gutterBottom component="div" sx={{ marginBottom: "20px" }}>
+            Specs
+          </Typography>
+          <Box sx={{ marginBottom: "40px" }}><GameServerBuildSpec gsb={gsb} /></Box>
+          <Typography variant="h6" gutterBottom component="div" sx={{ marginBottom: "20px" }}>
+            Patch Specs
+          </Typography>
+          <Box sx={{ marginBottom: "40px" }}><SpecForm clusterApi={clusterApi} gsb={gsb} /></Box>
+          <Typography variant="h6" gutterBottom component="div" sx={{ marginBottom: "20px" }}>
+            Status
+          </Typography>
+          <Box sx={{ marginBottom: "40px" }}><GameServerBuildStatus gsb={gsb} /></Box>
+          <Typography variant="h6" gutterBottom component="div" sx={{ marginBottom: "20px" }}>
+            Nodes
+          </Typography>
+          <Box sx={{ marginBottom: "40px" }}><NodeTable nodeData={nodeData} /></Box>
+          <Typography variant="h6" gutterBottom component="div" sx={{ marginBottom: "20px" }}>
+            Game Servers
+          </Typography>
+          <Box><AllocateForm allocateApi={allocateApi} buildID={gsb.spec ? gsb.spec.buildID : ""} /></Box>
+          <Box><GameServerTable gsList={gsList} gsdByName={gsdByName} /></Box>
+        </React.Fragment>
+      }
+    </React.Fragment>
   );
 }
 
