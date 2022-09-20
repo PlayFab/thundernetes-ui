18c53f93289aa79fa165ba879977f914b2e617f6
ShrayRastogi <shray45@gmail.com>
2022-09-20 12:31:51 +0530

    added navigation to cluster item

--- src/Home/ClustersSummaryItem.tsx
+++ src/Home/ClustersSummaryItem.tsx
@@ -1,13 +1,14 @@
 import { TableCell, TableRow } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 interface ClustersSummaryItemProps {
   clusterName: string,
   values: Record<string, number>
 }
-
 function ClustersSummaryItem({ clusterName, values }: ClustersSummaryItemProps) {
+  const navigate = useNavigate();
   return (
-    <TableRow>
+    <TableRow onClick={() => navigate(clusterName)} hover style={{cursor: 'pointer'}}>
       <TableCell>{clusterName}</TableCell>
       <TableCell>{values.standingBy}</TableCell>
       <TableCell>{values.active}</TableCell>
