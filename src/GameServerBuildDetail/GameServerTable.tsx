eb03ef2c3149aa14369a354ac3e7e48c3464981e
ShrayRastogi <shray45@gmail.com>
2022-09-20 22:32:58 +0530

    added sort by build name

--- src/GameServerBuildDetail/GameServerTable.tsx
+++ src/GameServerBuildDetail/GameServerTable.tsx
@@ -1,6 +1,6 @@
 import { useMemo, useState } from "react";
 import GameServerTableItem from "./GameServerTableItem";
-import { Box, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TablePagination, TableRow, Paper } from "@mui/material";
+import { Box, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TablePagination, TableRow, Paper, TableSortLabel } from "@mui/material";
 import { GameServer } from "../types";
 import TablePaginationActions from "../Common/TablePaginationActions";
 import { visuallyHidden } from '@mui/utils';
@@ -13,6 +13,7 @@ interface GameServerTableProps {
 
 function GameServerTable({ clusterApi, gsList, gsdByName }: GameServerTableProps) {
   const [page, setPage] = useState(0);
+  const [sortOrder, setSortOrder] = useState("asc");
   const rowsPerPage = 10;
 
   const emptyRows =
@@ -25,31 +26,66 @@ function GameServerTable({ clusterApi, gsList, gsdByName }: GameServerTableProps
     setPage(newPage);
   };
 
+  function descendingComparator(a: GameServer, b: GameServer) {
+    if (b.metadata.name < a.metadata.name) {
+      return -1;
+    }
+    if (b.metadata.name > a.metadata.name) {
+      return 1;
+    }
+    return 0;
+  }
+
+  const sortHandler =
+    () => (event: React.MouseEvent<unknown>) => {
+      const isAsc = sortOrder === "asc";
+      setSortOrder(isAsc ? "desc" : "asc");
+    };
+
   const items = useMemo(() => {
-    const sortedGs = gsList.sort((a: GameServer, b: GameServer) => a.metadata.name > b.metadata.name ? 1 : (a.metadata.name < b.metadata.name ? -1 : 0));
+    function getComparator(
+      order: string
+    ): (
+      a: GameServer,
+      b: GameServer
+    ) => number {
+      return order === 'desc'
+        ? (a, b) => descendingComparator(a, b)
+        : (a, b) => -descendingComparator(a, b);
+    }
+    const sortedGs = gsList.sort(getComparator(sortOrder));
     return sortedGs.map((gs, index) => <GameServerTableItem key={index} clusterApi={clusterApi} gs={gs} gsd={gsdByName[gs.metadata.name]} />);
-  }, [clusterApi, gsList, gsdByName]);
+  }, [clusterApi, gsList, gsdByName, sortOrder]);
 
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 500 }}>
         <TableHead>
           <TableRow>
-            <TableCell>Name</TableCell>
+            <TableCell sortDirection={sortOrder === "asc" ? "asc" : "desc"}>
+              <TableSortLabel
+                active={true}
+                direction={sortOrder === "asc" ? "asc" : "desc"}
+                onClick={sortHandler()}
+              >
+                Name
+              </TableSortLabel>
+            </TableCell>
             <TableCell>Namespace</TableCell>
             <TableCell>Health</TableCell>
             <TableCell>State</TableCell>
             <TableCell>Public IP</TableCell>
             <TableCell>Ports</TableCell>
             <TableCell>Player Count</TableCell>
-            <TableCell aria-label="Actions"><Box sx={visuallyHidden}>Actions</Box></TableCell>
+            <TableCell aria-label="Actions">
+              <Box sx={visuallyHidden}>Actions</Box>
+            </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {(rowsPerPage > 0
+          {rowsPerPage > 0
             ? items.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-            : items
-          )}
+            : items}
           {emptyRows > 0 && (
             <TableRow style={{ height: 53 * emptyRows }}>
               <TableCell colSpan={7} />
@@ -66,7 +102,7 @@ function GameServerTable({ clusterApi, gsList, gsdByName }: GameServerTableProps
               page={page}
               SelectProps={{
                 inputProps: {
-                  'aria-label': 'rows per page',
+                  "aria-label": "rows per page",
                 },
                 native: true,
               }}
