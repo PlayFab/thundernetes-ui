fa98157732b221367bf5248590e437d6972453f0
Imad Sheriff <imadsheriff97@gmail.com>
2022-09-21 22:55:46 -0700

    Added Feature to display Node Capacity

--- src/Common/NodeTableItem.tsx
+++ src/Common/NodeTableItem.tsx
@@ -1,14 +1,22 @@
-import { TableCell, TableRow } from "@mui/material";
+import { Chip, TableCell, TableRow } from "@mui/material";
+import React from "react";
 
 interface NodeTableItemProps {
   nodeName: string,
-  values: Record<string, number>
+  values: Record<string, number>,
+  selectedNode?: string,
+  onNodeSelected: (name: string, data: Record<string, number>) => void
 }
 
-function NodeTableItem({ nodeName, values }: NodeTableItemProps) {
+
+function NodeTableItem({ nodeName, values, onNodeSelected, selectedNode }: NodeTableItemProps) {
+  const [isHovered, setIsHovered] = React.useState(false);
   return (
-    <TableRow>
-        <TableCell>{nodeName}</TableCell>
+    <TableRow sx={[{ '&:hover': { cursor: 'pointer' } }]} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} hover selected={nodeName === selectedNode} onClick={() => onNodeSelected(nodeName, values)}>
+        <TableCell style={{ display: 'flex', alignItems: 'center', width: '250px'}}>
+          <span style={{ marginRight: '5px' }}>{nodeName}</span>
+          { (isHovered || nodeName === selectedNode) && <Chip sx={{ fontSize: 'x-small' }} label="View Metrics" size="small" />} 
+        </TableCell>
         <TableCell>{values.active}</TableCell>
         <TableCell>{values.standingBy}</TableCell>
       </TableRow>
